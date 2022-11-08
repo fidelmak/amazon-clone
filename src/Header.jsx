f2ae11d69dc22d947c7756cf0be4dda9e1b3f348
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2022-11-08 06:00:35 +0100

    added new features

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export function Header(props) {
+    
+
+    return (
+        <nav className='header'>
+
+            <img className='header__logo' src="https://pngimg.com/uploads/amazon/amazon_PNG1.png" alt="amazon" />
+        </nav>
+    )
+}
