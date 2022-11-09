335c36ccb0e17dfd0665a4f090fd86c430d385d7
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2022-11-09 05:50:52 +0100

    latext

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import "./Header.css";
+import { Link } from 'react-router-dom';
+import Searchicon from "@material-ui/icons/Search";
 
 export function Header(props) {
     
 
     return (
         <nav className='header'>
-
+            <Link to="/">
             <img className='header__logo' src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazon" />
+            
+            </Link>
+
+            {/* search box */}
+            <div className="header__search">
+            <input type="text" className="header__searchinput" />
+            <Searchicon className="header__searchIcon" />
+            </div>
+            
+
+            
         </nav>
     )
 }
