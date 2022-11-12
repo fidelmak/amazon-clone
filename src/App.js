f807ebb177922dc9a4a85829985c1ca1989f6a0d
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2022-11-12 22:44:29 +0100

    new addition

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@
 import './App.css';
 import {BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 import { Header } from './Header';
+import Home from './Home';
 
 function App() {
   return (
     <Router>
       <div className="app">
       <Switch>
-        <Route path='/checkout'> <h1>Checkout</h1> </Route>
+        <Route path='/checkout'> <Header /><h1>Checkout</h1> </Route>
         <Route path='/login'> <h1>login page</h1> </Route>
         <Route path='/'> 
         
 
         <Header />
-        <h1>Home-Page</h1>
+        <Home />
         
          </Route>
       </Switch>
---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+        <img className='home__image' src="https://m.media-amazon.com/images/I/71aQ3u78A3L._SX3000_.jpg" alt="" />
+        
+    </div>
+  )
+}
+
+export default Home
