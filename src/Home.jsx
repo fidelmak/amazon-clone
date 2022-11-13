b222653bf3957e5cd58424951b54b10968c15f04
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2022-11-13 13:47:32 +0100

    new product

--- src/Home.jsx
+++ src/Home.jsx
@@ -156,6 +156,165 @@ function Home() {
         image="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/September/DashboardCards/Fuji_Dash_Pets_1X._SY304_CB639746743_.jpg" />
 
         </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <div className="home__row">
+        <Product
+        id="1"
+        title="The lean Startup the lean "
+        price={11.96} 
+        rating={5}
+        image="https://images-na.ssl-images-amazon.com/images/G/01/events/GFAH/GWDesktop_SingleImageCard_fitathome_1x._SY304_CB434924743_.jpg" />
+
+        <Product
+        id="1"
+        title="The lean Startup the lean "
+        price={11.96} 
+        rating={5}
+        image="https://images-na.ssl-images-amazon.com/images/G/01/DiscoTec/2022/HOL22/GW/CategoryCards/DT/HOL22_GW_DT_CatCard_Gifts-Under-10_1x._SY304_CB606833054_.jpg" />
+
+        <Product
+        id="1"
+        title="The lean Startup the lean "
+        price={11.96} 
+        rating={5}
+        image="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2022/February/DashboardCards/GW_CONS_AUS_HPC_HPCEssentials_CatCard_Desktop1x._SY304_CB627424361_.jpg" />
+
+
+        <Product
+        id="1"
+        title="the lean Startup the lean "
+        price={11.96} 
+        rating={5}
+        image="https://m.media-amazon.com/images/I/71LbB2+EOZL._AC_SY200_.jpg" />
+
+        </div>
+        <div className="home__row">
+
+        <Product
+        id="1"
+        title="the lean Startup the lean "
+        price={11.96} 
+        rating={5}
+        image="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_Laptops_379x304_1X_en_US._SY304_CB418608471_.jpg" />
+
+
+        <Product
+        id="1"
+        title="The lean Startup the lean "
+        price={11.96} 
+        rating={5}
+        image="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/September/DashboardCards/Fuji_Dash_Fitness_1X._SY304_CB639748186_.jpg" />
+
+         <Product
+        id="1"
+        title="The lean Startup the lean "
+        price={11.96} 
+        rating={5}
+        image="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/September/DashboardCards/Fuji_Dash_SmartWatch_1X._SY304_CB639922137_.jpg" />
+
+
+         <Product
+        id="1"
+        title="the lean Startup the lean "
+        price={11.96} 
+        rating={5}
+        image="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_Electronics_1x._SY304_CB432774322_.jpg" />
+
+        </div>
+        <div className="home__r">
+
+        <Product
+        id="1"
+        title="The lean Startup the lean "
+        price={11.96} 
+        rating={5}
+        image="https://m.media-amazon.com/images/I/71nEuBNdHSL._AC_SY200_.jpg"
+         />
+
+        
+
+
+        
+         
+
+        </div>
+        <div className="home__row">
+        <Product
+        id="1"
+        title="The lean Startup the lean "
+        price={11.96} 
+        rating={5}
+        image="https://m.media-amazon.com/images/I/81wm3qIEFvL._AC_UL320_.jpg" />
+
+        <Product
+        id="1"
+        title="The lean Startup the lean "
+        price={11.96} 
+        rating={5}
+        image="https://m.media-amazon.com/images/I/61n+vIfzOKL._AC_UL320_.jpg" />
+
+        <Product
+        id="1"
+        title="The lean Startup the lean "
+        price={11.96} 
+        rating={5}
+        image="https://m.media-amazon.com/images/I/91WqPt6KaUS._AC_UL320_.jpg" />
+
+
+        <Product
+        id="1"
+        title="the lean Startup the lean "
+        price={11.96} 
+        rating={5}
+        image="https://m.media-amazon.com/images/I/71k+7VRDLHL._AC_UL320_.jpg" />
+
+        </div>
+
+
+
+        <div className="home__row">
+        <Product
+        id="1"
+        title="The lean Startup the lean "
+        price={11.96} 
+        rating={5}
+        image="https://m.media-amazon.com/images/I/711SDEOjuyL._AC_UL320_.jpg" />
+
+        <Product
+        id="1"
+        title="The lean Startup the lean "
+        price={11.96} 
+        rating={5}
+        image="https://m.media-amazon.com/images/I/31jVGBflDRL._AC_UL320_.jpg" />
+
+        <Product
+        id="1"
+        title="The lean Startup the lean "
+        price={11.96} 
+        rating={5}
+        image="https://m.media-amazon.com/images/S/al-na-9d5791cf-3faf/8177440c-2632-4ef4-b44b-f9ab23d4a344._CR0,0,1200,628_SX460_QL70_.png" />
+
+
+        <Product
+        id="1"
+        title="the lean Startup the lean "
+        price={11.96} 
+        rating={5}
+        image="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/September/DashboardCards/Fuji_Dash_Pets_1X._SY304_CB639746743_.jpg" />
+
+        </div>
         
     </div>
   )
